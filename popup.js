cc0d1c90838c35254eb4254600f19d0b0e41734d
Anton <dubenchuk.anton@gmail.com>
2014-07-19 20:18:40 +0400

    update for updated Ya.Music

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
-	var loc = "javascript:(function(){if (Mu.Player.isPaused() || Mu.Player.isPlaying()) { if (Mu.Player.isPaused()) { Mu.Player.resume(); } else if (Mu.Player.isPlaying()) { Mu.Player.pause(); } };void(0)})();";
+	var loc = "javascript:(function(){$('.player-controls__btn_play').click();void(0)})();";
 	chrome.tabs.query({"url":"http://music.yandex.ru/*"}, function(ts) {
 		for (var i = 0; i < ts.length; i++) {
 			var t = ts[i];

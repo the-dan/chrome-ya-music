dbb8aedf7a2aebb56b49de64941dc482bb6bbcd1
Anton <dubenchuk.anton@gmail.com>
2015-03-18 22:20:17 +0300

    yandex.music moved to https

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
 	var loc = "javascript:(function(){$('.player-controls__btn_play').click();void(0)})();";
-	chrome.tabs.query({"url":"http://music.yandex.ru/*"}, function(ts) {
+	chrome.tabs.query({"url":"https://music.yandex.ru/*"}, function(ts) {
 		for (var i = 0; i < ts.length; i++) {
 			var t = ts[i];
 				chrome.tabs.update(t.id, { "url": loc });

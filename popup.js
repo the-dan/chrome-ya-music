316b5d16dabe1d361d40edcc7342e1317eac9c1d
the-dan <dubenchuk.anton@gmail.com>
2016-05-09 22:52:09 +0300

    press the first button found, not all at the same time

--- popup.js
+++ popup.js
@@ -1,20 +1,10 @@
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
-	var loc = "javascript:(function(){$('.player-controls__btn_play').click();void(0)})();";
+	var loc = "javascript:(function(){$('.player-controls__btn_play').first().click();void(0)})();";
 	chrome.tabs.query({"url":"https://music.yandex.ru/*"}, function(ts) {
-		for (var i = 0; i < ts.length; i++) {
-			var t = ts[i];
-				chrome.tabs.update(t.id, { "url": loc });
+		if (ts.length >= 1) {
+			var t = ts[0];
+			chrome.tabs.update(t.id, { "url": loc });
 		}
 	});
 });
-
-/*
-chrome.browserAction.setBadgeBackgroundColor({color:[0, 200, 0, 100]});
-
-var i = 0;
-window.setInterval(function() {
-  chrome.browserAction.setBadgeText({text:String(i)});
-  i++;
-}, 1000);
-*/
